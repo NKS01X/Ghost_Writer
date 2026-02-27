351ec52a6dd932228a09b5f5b797d954d743b807
NKS01X <nikhilayush2020@gmail.com>
2026-02-27 19:59:54 +0530

    Test-2

--- src/test2.java
+++ src/test2.java
@@ -3,12 +3,8 @@ import java.util.*; // Unused import (Checkstyle hates this)
 public class test2 
 { // Curly brace is on the wrong line
     public static void main(String[] args) {
-        System.out.println("Let's break the pipeline!");
-        
-int first_number=5; // Horrible indentation, underscores in name, no spaces around '='
-      int secondNumber = 10;
-      
-    System.out.println( Calculate(first_number,secondNumber) ); // Weird spacing
+        System.out.println("HEYYLO");
+    System.out.println("HEYGUFOEHO");
     }
 
     public static int Calculate(int a,int b) // Method name is capitalized, missing space after comma
